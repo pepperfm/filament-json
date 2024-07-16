f89fc599111773ff6278a02c949becc980b6eec9
PepperFM <Damon3453@yandex.ru>
2024-07-16 17:47:02 +0300

    fix(view): add asModal + asDrawer basis

--- resources/views/json.blade.php
+++ resources/views/json.blade.php
@@ -1,10 +1,128 @@
+{{--
+@php
+    use Filament\Support\Enums\Alignment;
+    use Filament\Support\Enums\FontFamily;
+    use Filament\Support\Enums\FontWeight;
+    use Filament\Support\Enums\IconPosition;
+    use Filament\Tables\Columns\TextColumn\TextColumnSize;
+
+    $alignment = $getAlignment();
+    $canWrap = $canWrap();
+    $descriptionAbove = $getDescriptionAbove();
+    $descriptionBelow = $getDescriptionBelow();
+    $iconPosition = $getIconPosition();
+    $isBadge = $isBadge();
+    $isBulleted = $isBulleted();
+    $isListWithLineBreaks = $isListWithLineBreaks();
+    $isLimitedListExpandable = $isLimitedListExpandable();
+
+    $url = $getUrl();
+
+    if (!$alignment instanceof Alignment) {
+        $alignment = filled($alignment) ? (Alignment::tryFrom($alignment) ?? $alignment) : null;
+    }
+
+    $arrayState = $getState();
+
+    if ($arrayState instanceof \Illuminate\Support\Collection) {
+        $arrayState = $arrayState->all();
+    }
+
+    $listLimit = 1;
+
+    if (is_array($arrayState)) {
+        if ($listLimit = $getListLimit()) {
+            $limitedArrayStateCount = (count($arrayState) > $listLimit) ? (count($arrayState) - $listLimit) : 0;
+
+            if (!$isListWithLineBreaks) {
+                $arrayState = array_slice($arrayState, 0, $listLimit);
+            }
+        }
+
+        $listLimit ??= count($arrayState);
+
+        if ((!$isListWithLineBreaks) && (!$isBadge)) {
+            $arrayState = implode(
+                ', ',
+                array_map(
+                    static fn($value) => $value instanceof \Filament\Support\Contracts\HasLabel ? $value->getLabel() : $value,
+                    $arrayState,
+                ),
+            );
+        }
+    }
+
+    $arrayState = \Illuminate\Support\Arr::wrap($arrayState);
+
+    /** @var \App\Filament\Admin\Resources\ActivityResource\Pages\ListActivities $page */
+    $page = $this;
+
+    $characterLimit = $getCharacterLimit();
+@endphp
 <div>
 @foreach($getState() as $variable => $value)
 @if(!empty($value))
     <p>
-        <b>{{ $variable }}</b>: {{ $value }}
+        <b>{{ $variable }}</b>: @if($characterLimit){{ str($value)->limit($characterLimit) }}@else{{ $value }}@endif
         &nbsp;
     </p>
 @endif
 @endforeach
 </div>
+--}}
+@php
+    // /** @var \App\Filament\Admin\Resources\ActivityResource\Pages\ListActivities $page */
+    //     $page = $this;
+        $characterLimit = $getCharacterLimit();
+        $asModal = $getAsModal();
+        $asDrawer = $getAsDrawer();
+@endphp
+@if($asModal)
+<x-filament::modal :slide-over="$asDrawer" width="xl">
+    <x-slot name="trigger">
+        <x-filament::icon-button icon="heroicon-m-sparkles" />
+    </x-slot>
+
+    <div class="my-2 text-sm tracking-tight">
+        @foreach($getState() as $key => $value)
+            <p class="my-3">
+            <span class="inline-block py-2 pr-1 mr-1 font-bold text-gray-700 whitespace-normal rounded-md dark:text-gray-200 bg-gray-500/10">
+                {{ $key }} :
+            </span>
+            @if(is_array($value))
+                <span class="whitespace-normal divide-x divide-gray-200 divide-solid dark:divide-gray-700">
+                @foreach ($value as $nestedKey => $nestedValue)
+                    <span class="inline-block mr-1">
+                        {{ $nestedKey }}: {{ is_array($nestedValue) ? json_encode($nestedValue) : $applyLimit($nestedValue) }}
+                    </span>
+                @endforeach
+                </span>
+            @else
+                <span class="whitespace-normal">{{ $applyLimit($value) }}</span>
+            @endif
+            </p>
+        @endforeach
+    </div>
+</x-filament::modal>
+@else
+<div class="my-2 text-sm tracking-tight">
+    @foreach($getState() as $key => $value)
+        <p class="my-3">
+            <span class="inline-block py-2 pr-1 mr-1 font-bold text-gray-700 whitespace-normal rounded-md dark:text-gray-200 bg-gray-500/10">
+                {{ $key }} :
+            </span>
+            @if(is_array($value))
+                <span class="whitespace-normal divide-x divide-gray-200 divide-solid dark:divide-gray-700">
+                @foreach ($value as $nestedKey => $nestedValue)
+                    <span class="inline-block mr-1">
+                        {{ $nestedKey }}: {{ is_array($nestedValue) ? json_encode($nestedValue) : $applyLimit($nestedValue) }}
+                    </span>
+                @endforeach
+                </span>
+            @else
+                <span class="whitespace-normal">{{ $applyLimit($value) }}</span>
+            @endif
+        </p>
+    @endforeach
+</div>
+@endif
--- src/Columns/JsonColumn.php
+++ src/Columns/JsonColumn.php
@@ -5,7 +5,152 @@ declare(strict_types=1);
 namespace PepperFM\FilamentJson\Columns;
 
 use Filament\Tables\Columns\TextColumn;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\Collection;
+use Illuminate\Support\HtmlString;
+use Illuminate\Support\Str;
 
 class JsonColumn extends TextColumn
 {
+    protected string $view = 'filament-json::json';
+
+    protected bool|\Closure $isHtml = true;
+
+    protected bool $asModal = false;
+
+    protected bool $asDrawer = false;
+
+    public function applyLimit(array|string|null $value): array|string|null|\Illuminate\Support\Stringable
+    {
+        if (is_string($value)) {
+            $characterLimit = $this->getCharacterLimit();
+
+            return $characterLimit ? str($value)->limit($characterLimit) : $value;
+        }
+
+        return $value;
+    }
+
+    public function asModal(bool $condition = true): static
+    {
+        $this->asModal = $condition;
+
+        return $this;
+    }
+
+    public function asDrawer(bool $condition = true): static
+    {
+        $this->asModal = $condition;
+        $this->asDrawer = $condition;
+
+        return $this;
+    }
+
+    public function getAsModal(): bool
+    {
+        return $this->evaluate($this->asModal);
+    }
+
+    public function getAsDrawer(): bool
+    {
+        return $this->evaluate($this->asDrawer);
+    }
+
+    public function getState(): mixed
+    {
+        if (!$this->getRecord()) {
+            return null;
+        }
+        $listLimit = $this->getListLimit();
+
+        $state = ($this->getStateUsing !== null) ?
+            $this->evaluate($this->getStateUsing) :
+            $this->getStateFromRecord();
+
+        if (is_string($state) && ($separator = $this->getSeparator())) {
+            $state = explode($separator, $state);
+            $state = (count($state) === 1 && blank($state[0])) ?
+                [] :
+                $state;
+        }
+
+        if (blank($state)) {
+            $state = $this->getDefaultState();
+        }
+
+        if ($state instanceof Collection) {
+            $state = $state->filter()->take($listLimit);
+        }
+        if (is_array($state)) {
+            $state = collect($state)->filter()->take($listLimit);
+        }
+
+        return $state;
+    }
+
+    // public function formatState(mixed $state): HtmlString
+    // {
+    //     $html = '';
+    //     $isHtml = $this->isHtml();
+    //     /** @var \Illuminate\Support\Collection $stateCollection */
+    //     $stateCollection = $this->getState()->sliding(1);
+    //     // $listLimit = $this->getListLimit();
+    //     // foreach (range(1, $listLimit) as $key => $listRow) {
+    //     foreach ($this->getState() as $key => $item) {
+    //         // $item = $stateCollection->get($key)->first();
+    //         // $jsonKey = $stateCollection->get($key)->keys()->first();
+    //         $newState = $this->evaluate($this->formatStateUsing ?? $item, [
+    //             'state' => $item,
+    //         ]);
+    //
+    //         if ($characterLimit = $this->getCharacterLimit()) {
+    //             $newState = Str::limit($item, $characterLimit, $this->getCharacterLimitEnd());
+    //         }
+    //         if ($wordLimit = $this->getWordLimit()) {
+    //             $newState = Str::words($item, $wordLimit, $this->getWordLimitEnd());
+    //         }
+    //         if ($isHtml && $this->isMarkdown()) {
+    //             $newState = Str::markdown($item);
+    //         }
+    //
+    //         $prefix = $this->getPrefix();
+    //         $suffix = $this->getSuffix();
+    //
+    //         if (
+    //             (($prefix instanceof Htmlable) || ($suffix instanceof Htmlable)) &&
+    //             (!$isHtml)
+    //         ) {
+    //             $isHtml = true;
+    //             $newState = e($newState);
+    //         }
+    //         if (filled($prefix)) {
+    //             if ($prefix instanceof Htmlable) {
+    //                 $prefix = $prefix->toHtml();
+    //             } elseif ($isHtml) {
+    //                 $prefix = e($prefix);
+    //             }
+    //
+    //             $newState = $prefix . $newState;
+    //         }
+    //         if (filled($suffix)) {
+    //             if ($suffix instanceof Htmlable) {
+    //                 $suffix = $suffix->toHtml();
+    //             } elseif ($isHtml) {
+    //                 $suffix = e($suffix);
+    //             }
+    //
+    //             $newState .= $suffix;
+    //         }
+    //         if (!empty($newState)) {
+    //             $html .= "
+    //                 <p>
+    //                     <b>$key</b>: $newState
+    //                     &nbsp;
+    //                 </p>
+    //             ";
+    //         }
+    //     }
+    //
+    //     return new HtmlString($html);
+    // }
 }
