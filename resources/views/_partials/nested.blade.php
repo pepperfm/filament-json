d44e480c8dbced16cb64a44b58b6e1803bd49ee7
PepperFM <Damon3453@yandex.ru>
2025-03-22 22:10:54 +0300

    fix(core): displaying array only values

--- resources/views/_partials/nested.blade.php
+++ resources/views/_partials/nested.blade.php
@@ -17,19 +17,17 @@
                 'justify-center p-4 my-2 shadow border border-gray-300 dark:border-gray-600 rounded-md',
            ])
         >
-            <pre
-                @class([
-                    'block border-b border-gray-200 dark:border-gray-600' => !$isNestingEnabled,
-               ])
-            >{{ $nestedKey }}: </pre>
+            @if($loop->index !== $nestedKey)
+                <pre
+                    @class([
+                        'block border-b border-gray-200 dark:border-gray-600' => !$isNestingEnabled,
+                   ])
+                >{{ $nestedKey }}: </pre>
+            @endif
             @if (is_array($nestedValue) || $nestedValue instanceof \Illuminate\Contracts\Support\Arrayable)
                 @if ($isNestingEnabled)
                     <div class="p-2 border border-gray-200 dark:border-gray-600 rounded-md mt-2">
-                        @include('filament-json::_partials.nested', [
-                            'items' => $nestedValue,
-                            'depth' => $depth + 1,
-                            'maxDepth' => $maxDepth,
-                        ])
+                        @include('filament-json::_partials.nested', ['items' => $nestedValue, 'depth' => $depth + 1, 'maxDepth' => $maxDepth])
                     </div>
                 @else
                     <span class="text-gray-500">[Data truncated]</span>
