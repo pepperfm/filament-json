c0c46a428c308be96053fecbd3ec754e10e7fd39
PepperFM <Damon3453@yandex.ru>
2025-08-17 17:33:45 +0300

    fix tailwind.conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,5 @@ export default {
     darkMode: 'class',
     corePlugins: { preflight: false },
     content: [],
-    safelist: [
-        '!text-gray-700','!text-gray-900','dark:!text-gray-200','dark:!text-gray-100',
-        '!opacity-100',
-        '!border-gray-200','dark:!border-gray-600',
-    ],
     theme: { extend: {} },
 }
